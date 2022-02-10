0eb26472b41dada397df684df5a85c79ac471afa
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-02-10 13:11:38 +0100

    fixed the authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const {UnauthenticatedError} = require('../errors')
+const jwt = require('jsonwebtoken')
+
+const authentication = async(req,res,next)=>{
+    const { authToken } = req.headers.authorization
+    if(!authToken || !authToken.startsWith('Bearer ')){
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+    const token = authToken.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = {userID:payload.userID, name:payload.username}
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
+
+}
+
+module.exports = authentication;
