23da3f3f4ead71eea1eff1357bdf6960ef60171e
Toba <tobajamezmadamori@gmail.com>
2022-02-26 19:33:38 +0100

    added the remove-product-from-cart functionality

--- controllers/cart.js
+++ controllers/cart.js
@@ -23,6 +23,8 @@ const addToCart = async(req,res)=>{
     let cart = await Cart.findOne({ user })
     if(cart && cart.product.find(item => item.product_name === product_name)){
         return res.status(StatusCodes.OK).json({ msg:'This product is already in your cart' })
+    }else if(!cart){
+        res.status(StatusCodes.OK).json({ msg:'You dont have a cart yet...', cart:{} })
     }
 
     // verifying the quantity of that particular product remaining
@@ -51,10 +53,16 @@ const addToCart = async(req,res)=>{
 }
 
 
-// the frontend should make sure the user cannot reduce the amount of a particular product in the cart if its remaining only 1!!!!!!!!!!!!!!!
+// the frontend should make sure the user cannot reduce the amount of a particular product in the cart if its remaining only 1....!!!
 const updateCart = async(req,res)=>{
     const { user:{ userID:user }, params:{ id:product } } = req
     const { product_name, product_quantity } = req.body  // note that product_quantity values here should either be -negative or +positive
+
+    // checking if there is a cart and the product is already in the cart
+    let cart = await Cart.findOne({ user })
+    if(!cart || !cart.product.find(item => item.product_name === product_name)){
+        return res.status(StatusCodes.OK).json({ msg:'This product does not exist in the cart' })
+    }
 
     // verifying the quantity of that particular product remaining
     const prod = await Product.findById({ _id:product }) 
@@ -65,7 +73,7 @@ const updateCart = async(req,res)=>{
     }
 
     // updating the cart
-    let cart = await Cart.findOneAndUpdate({ user, 'product.productID':product},{ $inc:{'product.$.product_quantity':product_quantity}}, {new:true, runValidators:true})
+    cart = await Cart.findOneAndUpdate({ user, 'product.productID':product},{ $inc:{'product.$.product_quantity':product_quantity}}, {new:true, runValidators:true})
     await cart.save()
     
     return res.status(StatusCodes.OK).json({ cart })    
@@ -74,7 +82,31 @@ const updateCart = async(req,res)=>{
 
 
 const removeFromCart = async(req,res)=>{
-    res.status(StatusCodes.OK).json({ msg: 'remove a product from the cart' })
+    const { product_quantity, product_name } = req.body
+    const { params:{ id:product }, user:{ userID: user }} = req
+
+    // checking if there is a cart and the product is already in the cart
+    let cart = await Cart.findOne({ user })
+    if(!cart || !cart.product.find(item => item.product_name === product_name)){
+        return res.status(StatusCodes.OK).json({ msg:'This product does not exist in the cart' })
+    }
+
+    // adding the product_quantity to the original product
+    const prod = await Product.findById({ _id:product }) 
+
+    const newQuantity = prod.verifyQuantity(-Math.abs(product_quantity))
+    if(!newQuantity){
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg:'server error, please try again later...'})
+    }
+
+    // updating the cart
+    cart = await Cart.findOneAndUpdate({ user }, { $pull:{ product:{productID:product} }}, { new:true, runValidators:true })
+    await cart.save()
+    
+    // I can either delete the cart if there is only one product in the cart or just leave the empty cart to be 
+    // showing, not sure which to do
+
+    res.status(StatusCodes.OK).json({ cart })
 }
 
 const deleteCart = async(req,res)=>{
