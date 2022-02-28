832774226b4de78ecc5f11c4c84e328796866039
Toba <tobajamezmadamori@gmail.com>
2022-02-28 13:20:26 +0100

    added the necessary setup and security packages for deployment

--- app.js
+++ app.js
@@ -10,14 +10,41 @@ const prodRouter = require('./routes/products')
 const cartRouter = require('./routes/cart')
 const {getAllProducts} = require('./controllers/product')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
-//inbuilt middleware
-app.use(express.json())
+// Swagger documentation
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocumentation = YAML.load('./swagger.yaml')
+
 
 // custom built middleware
 const notFoundMiddleware = require('./middleware/notfound')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authenticateUser = require('./middleware/authentication')
+
+// extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter(
+  {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  }  
+))
+app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+// documentation route after deployment
+app.get('/',(req,res)=>{
+    res.send('<h2>E-commerce Api</h2><a href="/api-docs">Documentation</a>')
+  })
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocumentation))
 
 // routes
 app.use('/api/v1/auth', authRouter)
@@ -25,10 +52,6 @@ app.get('/api/v1/products/get-all-products', getAllProducts) // allows for prosp
 app.use('/api/v1/products',authenticateUser, prodRouter)
 app.use('/api/v1/cart',authenticateUser, cartRouter)
 
-// testing route
-app.get('/', (req,res)=>{
-    res.send('Welcome to my first application using Node')
-})
 
 app.use(errorHandlerMiddleware)
 app.use(notFoundMiddleware)
